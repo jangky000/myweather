6a1d09bb192e77161c6348313d0908295c2a2812
양원석 <wonseokyang@cita.co.kr>
2019-06-21 04:11:22 +0900

    add time-change

--- background.js
+++ background.js
@@ -21,6 +21,7 @@ const getState = (id) => {
   } else if (id < 800) {
     state = 'partlycloudy';
   }
+
   window.chrome.browserAction.setIcon({
     path: {
       16: `images/${state}16.png`,
@@ -30,18 +31,37 @@ const getState = (id) => {
       128: `images/${state}128.png`,
     },
   });
+
+  // 팝업 이미지 및 텍스트 가져오기
+  const divState = document.querySelector('.ico_state');
+  const divStateText = document.querySelector('.info_state');
+
+  if (divState !== null) {
+    divState.style.backgroundImage = `url('/images/${state}128.png')`;
+    divStateText.innerHTML = weatherData.stateText;
+  }
+
+  // 현재시간 가져오기
+  if (divStateText !== null && (weatherData.hours < 6 || weatherData.hours >= 18)) {
+    divStateText.style.color = '#fff';
+    document.body.style.backgroundColor = '#000';
+  }
 };
 
-const getWeather = () => {
+const setWeather = () => {
   async function success(position) {
     geoData.lat = position.coords.latitude;
     geoData.lon = position.coords.longitude;
     const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${geoData.lat}&lon=${geoData.lon}&appid=83541a6ffb1bc21fa3f75f474dfbc7ae&units=metric`);
     const res = await data.json();
+    console.log(res);
 
     weatherData.temp = Math.round(res.main.temp).toString();
     weatherData.state = res.weather[0].id;
-    weatherData.city = res.name;
+    weatherData.stateText = res.weather[0].main;
+    weatherData.hours = new Date().getHours();
+
+    // weatherData.city = res.name;
 
     getTemp(weatherData.temp);
     getState(weatherData.state);
@@ -56,4 +76,4 @@ const getWeather = () => {
     navigator.geolocation.getCurrentPosition(success, error);
   }
 };
-getWeather();
+setWeather();
--- popup.js
+++ popup.js
@@ -1,16 +1,3 @@
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
-const getCity = () => {
-  const newDiv = document.createElement('div');
-  const newContent = document.createTextNode(weatherData.city);
-  const divBackground = document.querySelector('.background');
-
-  newDiv.appendChild(newContent);
-  divBackground.appendChild(newDiv);
-};
-setTimeout(() => {
-  getWeather();
-  getCity(weatherData.city);  
-}, 2000);
